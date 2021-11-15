27b995371f2e5ae9097c84bd8553b437e9211cae
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-11-15 22:56:18 +0330

    commit (doesn't compile)

--- src/command_gen.rs
+++ src/command_gen.rs
@@ -1,5 +1,5 @@
 use std::{
-    fs::{create_dir_all, read_to_string},
+    fs::{create_dir_all, read_dir, read_to_string},
     path::PathBuf,
 };
 
@@ -11,11 +11,6 @@ pub fn gen(_options: GenOptions) {
     // Create cmake directory.
     let path = PathBuf::from(DIR_CMAKE);
     create_dir_all(path).expect(format!("Failed to create '{}' directory.", DIR_CMAKE).as_str());
-
-    // Read cmake template file.
-    let path = PathBuf::from(format!("{}/{}", DIR_TEMPLATES, FILE_TEMPLATE_CMAKE));
-    let template =
-        read_to_string(path).expect(format!("Failed to open '{}'.", FILE_TEMPLATE_CMAKE).as_str());
 
     // Read aban.toml file.
     let aban_toml = read_to_string(FILE_CONFIG_ABAN)
@@ -29,11 +24,36 @@ pub fn gen(_options: GenOptions) {
         )
         .as_str(),
     );
+
+    // Gather C modules information.
+    let aban_modules_information = {
+        // Search src-c directory for subdirectories.
+        let rd = read_dir(DIR_SRC_C).expect(format!("Failed to read '{}'.", DIR_SRC_C).as_str());
+
+        // Iterate trough src-c directories.
+        for res in rd {
+            if let Ok(entry) = res {
+                entry.
+            }
+        }
+
+        return ();
+    };
+
+    let aban = Aban {
+        config: aban_config,
+        modules: todo!(),
+    };
+
+    // Read cmake template file.
+    let path = PathBuf::from(format!("{}/{}", DIR_TEMPLATES, FILE_TEMPLATE_CMAKE));
+    let template =
+        read_to_string(path).expect(format!("Failed to open '{}'.", FILE_TEMPLATE_CMAKE).as_str());
 }
 
 struct Aban {
     config: AbanConfig,
-    modules: Vec<AbanModuleConfig>,
+    modules: Vec<AbanModule>,
 }
 
 #[derive(Deserialize)]
@@ -41,10 +61,11 @@ struct AbanConfig {
     name: String,
 }
 
+struct AbanModule {}
+
 #[derive(Deserialize)]
 struct AbanModuleConfig {
-    name: String,
-    os: AbanOSConfig,
+    os: Option<AbanOSConfig>,
 }
 
 #[derive(Deserialize)]
