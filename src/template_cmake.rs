c43cf179ebb52da1cb33ecfadf9d4f93aac8b09b
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-11-17 21:20:01 +0330

    Update template_cmake.rs

--- src/template_cmake.rs
+++ src/template_cmake.rs
@@ -3,9 +3,9 @@ cmake_minimum_required(VERSION 3.10)
 
 project({{project_name}} VERSION 0.1.0)
 
-set( CMAKE_RUNTIME_OUTPUT_DIRECTORY "${cmake_current_list_dir}/../bin" )
-set( CMAKE_LIBRARY_OUTPUT_DIRECTORY "${cmake_current_list_dir}/../bin" )
-set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${cmake_current_list_dir}/../bin" )
+set( CMAKE_RUNTIME_OUTPUT_DIRECTORY "../bin" )
+set( CMAKE_LIBRARY_OUTPUT_DIRECTORY "../bin" )
+set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY "../bin" )
 
 option(ABAN_WIN32_SUBSYSTEM_WINDOWS OFF)
 
